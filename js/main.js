6c9d2448c09178ceaa5a7568c235cca12506950e
KhuatCong <khuatphucong06092002@gmail.com>
2023-10-04 17:31:51 +0700

    hihihi

--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
-$(function(){
-    $('.user').click(function(){
+$(function () {
+    $('.user').click(function () {
         $('.action').toggle();
+    })
+
+    $('.list-category-home').slick({
+        slidesToShow: 7,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 5
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+        ]
+    });
+
+    $('.tintuc-slide-home').slick({
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScrol: 1,
+                }
+            },
+            {
+                breakpoint: 568,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScrol: 1,
+                }
+            }
+        ]
+    });
+
+    $('.list-sell-product').slick({
+        slidesToShow: 6,
+        slidesToScrol:1,
     })
 })
