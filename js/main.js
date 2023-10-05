bf14cb956bb29edf4752c34d00f77da88026f594
KhuatCong <khuatphucong06092002@gmail.com>
2023-10-05 12:01:35 +0700

    hehe

--- js/main.js
+++ js/main.js
@@ -43,6 +43,46 @@ $(function () {
 
     $('.list-sell-product').slick({
         slidesToShow: 6,
-        slidesToScrol:1,
+        slidesToScrol: 1,
+        speed: 800,
     })
+
+    $('.product-img-list').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider-for',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+      });
+
 })
+if ($('#back-to-top').length) {
+    var scrollTrigger = 100, //khoảng cách đến khi bắt đầu hiện nút (px)
+        backToTop = function () {
+            var scrollTop = $(window).scrollTop(); //lấy khoảng cách scroll hiện tại tính lên Top
+            if (scrollTop > scrollTrigger) {
+                $('#back-to-top').addClass('show');
+            } else {
+                $('#back-to-top').removeClass('show');
+            }
+        };
+    backToTop();
+    $(window).on('scroll', function () {
+        backToTop(); //gọi function 'backToTop()' mỗi khi người dùng cuộn trang
+    });
+    $('#back-to-top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 400); //thời gian kéo lên top tính bằng mili giây 
+    });
+
+}
